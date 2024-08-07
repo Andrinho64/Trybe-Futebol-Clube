8eba9610efb7d2363ac6eff07c88cf60919cf2c5
Andrew Caetano <andrewr_caetano@hotmail.com>
2024-08-07 20:46:30 -0300

    refactor: Substituido o forEach para filter, reduce e map.

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -19,19 +19,17 @@ interface TeamStats {
 export default class LeaderboardService {
   private static calculatePointsAndResults(matches: MatchesModel[], teamId: number)
     : Pick<TeamStats, 'totalPoints' | 'totalVictories' | 'totalDraws' | 'totalLosses'> {
-    const pointAndResult: Pick<TeamStats, 'totalPoints' | 'totalVictories' | 'totalDraws'
-    | 'totalLosses'> = { totalPoints: 0, totalVictories: 0, totalDraws: 0, totalLosses: 0 };
-
-    matches.forEach((match) => {
-      if (match.homeTeamId === teamId) {
+    return matches
+      .filter((match) => match.homeTeamId === teamId)
+      .reduce((acc, match) => {
         if (match.homeTeamGoals > match.awayTeamGoals) {
-          pointAndResult.totalVictories += 1;
-          pointAndResult.totalPoints += 3;
+          acc.totalVictories += 1;
+          acc.totalPoints += 3;
         } else if (match.homeTeamGoals === match.awayTeamGoals) {
-          pointAndResult.totalDraws += 1;
-          pointAndResult.totalPoints += 1;
+          acc.totalDraws += 1;
+          acc.totalPoints += 1;
         } else {
-          pointAndResult.totalLosses += 1;
+          acc.totalLosses += 1;
         }
         return acc;
       }, { totalPoints: 0, totalVictories: 0, totalDraws: 0, totalLosses: 0 });
@@ -53,44 +51,29 @@ export default class LeaderboardService {
     const { totalPoints, totalVictories, totalDraws, totalLosses } = this
       .calculatePointsAndResults(matches, teamId);
     const { goalsFavor, goalsOwn } = this.calculateGoals(matches, teamId);
+    const totalGames = totalVictories + totalDraws + totalLosses;
     return {
       name,
       totalPoints,
       totalVictories,
       totalDraws,
       totalLosses,
-      goalsBalance: this.calculateGoalsBalance({ goalsFavor, goalsOwn }),
-      efficiency: this.calculateEfficiency({ totalPoints,
-        totalGames: totalVictories + totalDraws + totalLosses }),
-      totalGames: totalVictories + totalDraws + totalLosses,
+      goalsBalance: goalsFavor - goalsOwn,
+      totalGames,
       goalsFavor,
       goalsOwn,
+      efficiency: ((totalPoints / (totalGames * 3)) * 100).toFixed(2),
     } as TeamStats;
-  }
-
-  private static calculateGoalsBalance(stats: Pick<TeamStats, 'goalsFavor' | 'goalsOwn' >): number {
-    return stats.goalsFavor - stats.goalsOwn;
-  }
-
-  private static calculateEfficiency(stats: Pick<TeamStats, 'totalPoints' | 'totalGames' >)
-    : string {
-    const efficiency = (stats.totalPoints / (stats.totalGames * 3)) * 100;
-    return efficiency.toFixed(2);
   }
 
   public static async getHomeLeaderboard(): Promise<ServiceResponse<any, any>> {
     try {
       const teams = await TeamsModel.findAll();
-      const matches = await MatchesModel.findAll({
-        where: { inProgress: false },
-      });
+      const matches = await MatchesModel.findAll({ where: { inProgress: false } });
 
-      const leaderboard = teams.map((team) => {
-        const stats = this.calculateTeamStats(matches, team.id, team.teamName);
-        return stats;
-      });
-      leaderboard.sort(this.compareTeams);
-
+      const leaderboard: TeamStats[] = teams
+        .map((team) => this.calculateTeamStats(matches, team.id, team.teamName))
+        .sort(this.compareTeams);
       return { status: HTTP_STATUS.OK, data: leaderboard };
     } catch (error) {
       return { status: HTTP_STATUS.INTERNAL_SERVER_ERROR,
